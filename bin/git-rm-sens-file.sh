a6333d293aee3c3b3c21c32106e6a459a6e33403
paul wisehart <paul@oldcode.org>
2020-03-28 22:41:28 -0400

    __gca__

---
+++ bin/git-rm-sens-file.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#git filter-branch --force --index-filter \
+#  "git rm --cached --ignore-unmatch PATH-TO-YOUR-FILE-WITH-SENSITIVE-DATA" \
+#  --prune-empty --tag-name-filter cat -- --all
+
+if [[ $# -lt 1 ]] ; then
+	echo "$0 <file-to-remove-all-traces-of>"
+	exit 1
+fi
+
+f=$1
+
+git filter-branch --force --index-filter \
+	"git rm --cached --ignore-unmatch ${1}" \
+	--prune-empty --tag-name-filter cat -- --all
+
